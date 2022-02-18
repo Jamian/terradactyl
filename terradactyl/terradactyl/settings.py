cb2ae33b5d886f37a1ac1a64e34df733906b8437
Jamie West <jamieianwest@hotmail.com>
2022-02-18 10:13:20 +0000

    ci: removing known HIGH SEV used to test Bandit CI

--- terradactyl/terradactyl/settings.py
+++ terradactyl/terradactyl/settings.py
@@ -22,9 +22,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
-ssl.wrap_socket(ssl_version=ssl.PROTOCOL_SSLv3)
-
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
